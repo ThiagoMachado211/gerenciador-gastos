8b7c52b04c95dd425e2a9054ad3f8d1db1251a01
ThiagoMachado211 <thiagao.matematica@gmail.com>
2026-04-10 23:10:47 -0300

    Corrige sintaxe do production.rb 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 require "active_support/core_ext/integer/time"
 
-Rails.application.configure do require
+Rails.application.configure do
   config.action_cable.mount_path = "/cable"
   config.action_controller.perform_caching = true
   config.action_mailer.default_url_options = { host: "example.com" }
